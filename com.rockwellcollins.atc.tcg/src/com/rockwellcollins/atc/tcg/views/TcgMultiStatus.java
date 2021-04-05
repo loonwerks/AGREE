b13025fffba612aefaf2e1d7f9aa75f7ea699afc
Karl Hoech <karl.hoech@collins.com>
2021-04-05 14:30:05 -0500

    Add copyright/license to AGREE Test Case Generator sources

--- com.rockwellcollins.atc.tcg/src/com/rockwellcollins/atc/tcg/views/TcgMultiStatus.java
+++ com.rockwellcollins.atc.tcg/src/com/rockwellcollins/atc/tcg/views/TcgMultiStatus.java
@@ -1,3 +1,23 @@
+/*
+ * Copyright (c) 2021, Collins Aerospace.
+ * Developed with the sponsorship of Defense Advanced Research Projects Agency (DARPA).
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this data,
+ * including any software or models in source or binary form, as well as any drawings, specifications,
+ * and documentation (collectively "the Data"), to deal in the Data without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Data, and to permit persons to whom the Data is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Data.
+ *
+ * THE DATA IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+ * LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS, SPONSORS, DEVELOPERS, CONTRIBUTORS, OR COPYRIGHT HOLDERS BE LIABLE
+ * FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE DATA OR THE USE OR OTHER DEALINGS IN THE DATA.
+ */
 package com.rockwellcollins.atc.tcg.views;
 
 import java.util.StringJoiner;
@@ -7,7 +27,7 @@ import jkind.api.results.Status;
 
 public class TcgMultiStatus {
 	final private MultiStatus ms;
-	
+
 	public TcgMultiStatus(MultiStatus ms) {
 		this.ms = ms;
 	}
@@ -48,11 +68,11 @@ public class TcgMultiStatus {
 	public String tcgStatus(Status status) {
 		switch (status) {
 		case INVALID: return "test case found" ;
-		case VALID: return "no test case possible" ; 
+		case VALID: return "no test case possible" ;
 		default: return status.toString() ;
 		}
 	}
-	
+
 	@Override
 	public String toString() {
 		StringJoiner text = new StringJoiner(", ");
--- com.rockwellcollins.atc.tcg/src/com/rockwellcollins/atc/tcg/views/TcgResultTree.java
+++ com.rockwellcollins.atc.tcg/src/com/rockwellcollins/atc/tcg/views/TcgResultTree.java
@@ -1,3 +1,23 @@
+/*
+ * Copyright (c) 2021, Collins Aerospace.
+ * Developed with the sponsorship of Defense Advanced Research Projects Agency (DARPA).
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this data,
+ * including any software or models in source or binary form, as well as any drawings, specifications,
+ * and documentation (collectively "the Data"), to deal in the Data without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Data, and to permit persons to whom the Data is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Data.
+ *
+ * THE DATA IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+ * LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS, SPONSORS, DEVELOPERS, CONTRIBUTORS, OR COPYRIGHT HOLDERS BE LIABLE
+ * FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE DATA OR THE USE OR OTHER DEALINGS IN THE DATA.
+ */
 package com.rockwellcollins.atc.tcg.views;
 
 import org.eclipse.jface.layout.TreeColumnLayout;
@@ -12,7 +32,7 @@ import jkind.api.ui.results.AnalysisResultColumnViewer;
 
 public class TcgResultTree extends AnalysisResultColumnViewer {
 	private TreeViewer treeViewer;
-	
+
 	public TcgResultTree(Composite parent) {
 		super(parent);
 	}
@@ -24,7 +44,7 @@ public class TcgResultTree extends AnalysisResultColumnViewer {
 		createColumns();
 		return treeViewer;
 	}
-	
+
 	private void createColumns() {
 		TreeViewerColumn propertyColumn = new TreeViewerColumn(treeViewer, SWT.None);
 		propertyColumn.getColumn().setText("Test Spec");
@@ -36,13 +56,13 @@ public class TcgResultTree extends AnalysisResultColumnViewer {
 		resultColumn.getColumn().setText("Result");
 //		resultColumn.setLabelProvider(new AnalysisResultLabelProvider(Column.RESULT));
 		resultColumn.setLabelProvider(new TestCaseGeneratorResultLabelProvider(Column.RESULT));
-		
+
 		TreeColumnLayout layout = new TreeColumnLayout();
 		composite.setLayout(layout);
 		layout.setColumnData(propertyColumn.getColumn(), new ColumnWeightData(2));
 		layout.setColumnData(resultColumn.getColumn(), new ColumnWeightData(1));
 	}
-	
+
 	@Override
 	public TreeViewer getViewer() {
 		return treeViewer;

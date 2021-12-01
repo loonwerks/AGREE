b11082b5ee7f684b70588875d7348bbb02d70868
Karl Hoech <karl.hoech@collins.com>
2021-12-01 05:56:22 -0600

    Collect obligations during analysis

--- com.rockwellcollins.atc.tcg/src/com/rockwellcollins/atc/tcg/suite/TestSuiteUtils.java
+++ com.rockwellcollins.atc.tcg/src/com/rockwellcollins/atc/tcg/suite/TestSuiteUtils.java
@@ -2,20 +2,20 @@
 Copyright (c) 2016, Rockwell Collins.
 Developed with the sponsorship of Defense Advanced Research Projects Agency (DARPA).
 
-Permission is hereby granted, free of charge, to any person obtaining a copy of this data, 
-including any software or models in source or binary form, as well as any drawings, specifications, 
+Permission is hereby granted, free of charge, to any person obtaining a copy of this data,
+including any software or models in source or binary form, as well as any drawings, specifications,
 and documentation (collectively "the Data"), to deal in the Data without restriction, including
-without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
-and/or sell copies of the Data, and to permit persons to whom the Data is furnished to do so, 
+without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Data, and to permit persons to whom the Data is furnished to do so,
 subject to the following conditions:
 
-The above copyright notice and this permission notice shall be included in all copies or 
+The above copyright notice and this permission notice shall be included in all copies or
 substantial portions of the Data.
 
-THE DATA IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT 
-LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
-IN NO EVENT SHALL THE AUTHORS, SPONSORS, DEVELOPERS, CONTRIBUTORS, OR COPYRIGHT HOLDERS BE LIABLE 
-FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, 
+THE DATA IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS, SPONSORS, DEVELOPERS, CONTRIBUTORS, OR COPYRIGHT HOLDERS BE LIABLE
+FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
 ARISING FROM, OUT OF OR IN CONNECTION WITH THE DATA OR THE USE OR OTHER DEALINGS IN THE DATA.
 */
 
@@ -26,31 +26,36 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import com.rockwellcollins.atc.tcg.obligations.ufc.Obligation;
+import com.rockwellcollins.atc.tcg.obligations.ufc.TcgRenaming;
+
 import jkind.api.results.JKindResult;
 import jkind.api.results.PropertyResult;
 import jkind.results.InvalidProperty;
 
 public class TestSuiteUtils {
 
-	public static TestSuite testSuiteFromJKindResult(JKindResult result, String implUnderTest, String name, String description) {
+	public static TestSuite testSuiteFromJKindResult(JKindResult result, String implUnderTest, String name,
+			String description, TcgRenaming tcgRenaming) {
 		List<TestCase> tests = new ArrayList<>();
 		for (PropertyResult pr : result.getPropertyResults()) {
 			if (pr.getProperty() instanceof InvalidProperty) {
 				InvalidProperty invalidProp = (InvalidProperty)pr.getProperty();
-				tests.add(new TestCase(pr.getName(), "", invalidProp.getCounterexample()));
+				Set<Obligation> satisfiedObligations = tcgRenaming.statisfiedObligations(pr.getName());
+				tests.add(new TestCase(pr.getName(), "", invalidProp.getCounterexample(), satisfiedObligations));
 			}
 		}
-		return new TestSuite(implUnderTest, name, description, tests, TestSuite.State.INSTANTIATED); 
+		return new TestSuite(implUnderTest, name, description, tests, TestSuite.State.INSTANTIATED);
 	}
-	
+
 	public static <T> Set<T> safeSet(Set<T> theSet) {
 		return (null == theSet) ? new HashSet<T>() : theSet;
 	}
-	
+
 	public static <T> List<T> safeList(List<T> theList) {
 		return (null == theList) ? new ArrayList<T>() : theList;
 	}
-	
+
 	public static String safeString(String s) {
 		return (null == s) ? "" : s;
 	}

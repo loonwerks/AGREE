85e4650ccd9e5bcc05967e4e3da7927f8dab6c31
Karl Hoech <karl.hoech@collins.com>
2022-02-22 06:54:06 -0600

    Base our resource description strategy on Xtext default

--- com.rockwellcollins.atc.agree/src/com/rockwellcollins/atc/agree/scoping/AgreeResourceDescriptionStrategy.java
+++ com.rockwellcollins.atc.agree/src/com/rockwellcollins/atc/agree/scoping/AgreeResourceDescriptionStrategy.java
@@ -1,11 +1,9 @@
 package com.rockwellcollins.atc.agree.scoping;
 
-import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.xtext.resource.IEObjectDescription;
-import org.eclipse.xtext.resource.IReferenceDescription;
+import org.eclipse.xtext.resource.impl.DefaultResourceDescriptionStrategy;
 import org.eclipse.xtext.util.IAcceptor;
-import org.osate.xtext.aadl2.scoping.Aadl2ResourceDescriptionStrategy;
 
 import com.google.inject.Singleton;
 import com.rockwellcollins.atc.agree.agree.ExistsExpr;
@@ -18,7 +16,7 @@ import com.rockwellcollins.atc.agree.agree.ForallExpr;
  * @since 3.0
  */
 @Singleton
-public class AgreeResourceDescriptionStrategy extends Aadl2ResourceDescriptionStrategy {
+public class AgreeResourceDescriptionStrategy extends DefaultResourceDescriptionStrategy {
 
 	public AgreeResourceDescriptionStrategy() {
 		super();
@@ -40,13 +38,7 @@ public class AgreeResourceDescriptionStrategy extends Aadl2ResourceDescriptionSt
 				|| ((FoldLeftExpr) container).getBinding().equals(eObject))) {
 			return false;
 		}
-		return defaultCreateEObjectDescriptions(eObject, acceptor);
-	}
-
-	@Override
-	public boolean createReferenceDescriptions(EObject eObject, URI exportedContainerURI,
-			IAcceptor<IReferenceDescription> acceptor) {
-		return defaultCreateReferenceDescriptions(eObject, exportedContainerURI, acceptor);
+		return super.createEObjectDescriptions(eObject, acceptor);
 	}
 
 }

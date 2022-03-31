99334c22779a7f07ff79bfb9710b1c107ffdbd89
Karl Hoech <karl.hoech@collins.com>
2022-03-31 14:30:53 -0500

    Improve changelog builder

--- agree-releng/scripts/changelog-builder.py
+++ agree-releng/scripts/changelog-builder.py
@@ -20,7 +20,7 @@ for label in VALID_LABELS:
 ########### API REQUEST ###########
 
 http = urllib3.PoolManager(10, headers={'user-agent': 'Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0'}, cert_reqs='CERT_REQUIRED', ca_certs=certifi.where())
-r = http.request('GET', 'https://api.github.com/repos/loonwerks/agree/issues?milestone=1&state=closed&per_page=80')
+r = http.request('GET', 'https://api.github.com/repos/loonwerks/agree/issues?milestone=2&state=closed&per_page=80')
 
 ########### BUILDING THE TITLES DATA STRUCTURE ###########
 
@@ -28,11 +28,11 @@ data = json.loads(r.data.decode('utf-8'))
 for elem in data:
     for label in elem['labels']:
         if label['name'] in VALID_LABELS:
-            titles[label['name']].append(elem['title'] + '\n       (`' + str(elem['number']) + ' <https://github.com/loonwerks/agree/issues/' + str(elem['number']) + '>`__)')
+            titles[label['name']].append(elem['title'] + ' ([' + str(elem['number']) + '](https://github.com/loonwerks/agree/issues/' + str(elem['number']) + '))')
 
 ########### PRINTING THE OUTPUT ###########
 
-print('#### Fixed issues\n')
+print('## Fixed issues\n')
 
 for label in VALID_LABELS:
     i = 1
@@ -40,5 +40,5 @@ for label in VALID_LABELS:
         continue
     print(' * ' + label)
     for title in reversed(titles[label]):
-        print('   ' + str(i) + '.  ' + title)
+        print('   - ' + title)
         i += 1
